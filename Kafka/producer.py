d405c022b5657c8c0be020a99dd1e6a7452a4bfb
jaiGit2022 <jkishan421@gmail.com>
2021-02-10 10:36:26 +0500

    add readme and requirements.txt

--- Kafka/producer.py
+++ Kafka/producer.py
@@ -11,7 +11,8 @@ print("producer sending data on topic : " + topic_name + " .......")
 for i in range(int(records)):
     key = "key" + str(i + 1)
     value = "value" + str(i + 1)
-    future = producer.send(topic_name, key=str.encode(key), value=str.encode(value))
+    future = producer.send(topic_name,
+                           key=str.encode(key), value=str.encode(value))
 
 future = producer.send(topic_name, b'stop')
 
